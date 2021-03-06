4f31e8a2b2eca6160fce7ed81af289e177393730
fabio-elioterio <fabioelioterio.77@gmail.com>
2021-03-05 22:42:01 -0300

    project iniciation with user

---
+++ src/entities/UserEntity.kts
@@ -0,0 +1,9 @@
+data class(
+        private val userId: Long,
+        private val name: String,
+        private val document: String,
+        private val documentType: DocumentType,
+        private val grossSalary: Float,
+        private val netSalary: Float,
+        private val occupation: String
+)
